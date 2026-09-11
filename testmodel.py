32de742b1599ced1c77a2023c02b69e0056f3672
wissemammar764-a11 <wissemammar764@gmail.com>
2026-09-11 15:48:02 +0200

    fix: clé API sécurisée via .env, ajout gitignore

--- testmodel.py
+++ testmodel.py
@@ -1,13 +1,15 @@
+import os
+from dotenv import load_dotenv
 from google import genai
 
-API_KEY = "AQ.Ab8RN6KIfJNXTHc0MqOTT46Kv51zxnJiBefQuBzAjg9_FPyURQ"
+load_dotenv()
+API_KEY = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=API_KEY)
 
 print("\nModèles testés :\n")
 
 for model in client.models.list():
-
     actions = getattr(model, "supported_actions", [])
 
     if "generateContent" not in actions:
@@ -18,7 +20,6 @@ for model in client.models.list():
             model=model.name,
             contents="Dis juste OK"
         )
-
         print(f" {model.name}")
 
     except Exception as e:
